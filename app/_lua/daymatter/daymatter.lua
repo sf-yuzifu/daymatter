0ba88f9937363d9b447a91283ddc70e08c6d23eb
sf-yuzifu <yuzifu@yuzifu.top>
2024-12-22 00:07:33 +0800

    对接快应用

--- app/_lua/daymatter/daymatter.lua
+++ app/_lua/daymatter/daymatter.lua
@@ -8,6 +8,41 @@ local STATE_POSITION_UP = 1
 local STATE_POSITION_MID = 2
 local STATE_POSITION_BOTTOM = 3
 
+local file = '/data/quickapp/files/com.yzf.daymatter/date.txt'
+
+local function fileExists(name)
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
+
+local function splitString(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
+local function readFileToStr(file)
+    if fileExists(file) == false then
+        printf('File Not Found : ' .. file)
+        return ''
+    end
+    local text = ''
+    for f in io.lines(file) do
+        text = text .. f .. '\n'
+    end
+    return text
+end
+
 local printf = DEBUG_ENABLE and print or function(...)
 end
 
@@ -70,6 +105,60 @@ local function Image(root, src, pos)
 
     return t
 end
+
+
+local isS3 = fileExists('/font/MiSans-Demibold.ttf')
+
+local TEXT_FONT = lvgl.BUILTIN_FONT.MONTSERRAT_14
+
+function FontChange(font1, font2)
+    if DEBUG_ENABLE == false then
+        if isS3 then
+            TEXT_FONT = lvgl.Font('MiSans-' .. font2 .. '', 24)
+        else
+            TEXT_FONT = lvgl.Font('misansw_' .. font1 .. '', 24)
+        end
+    end
+end
+
+FontChange("demibold", "Demibold")
+
+local function Label(root, text, pos, color)
+    --- @class Label
+    local t = {} -- create new table
+
+    t.widget = lvgl.Object(root, {
+        outline_width = 0,
+        border_width = 0,
+        pad_all = 0,
+        bg_opa = 0,
+        bg_color = 0,
+        w = pos.w,
+        h = 490,
+        align = lvgl.ALIGN.CENTER
+    })
+    t.widget:clear_flag(lvgl.FLAG.SCROLLABLE)
+    t.widget:add_flag(lvgl.FLAG.EVENT_BUBBLE)
+
+    t.label = t.widget:Label {
+        w = pos.w,
+        -- h = 50,
+        x = 0,
+        y = pos.y,
+        text = text,
+        text_color = color,
+        font_size = 24,
+        text_font = TEXT_FONT,
+        text_align = lvgl.ALIGN.TOP_MID,
+        long_mode = lvgl.LABEL.LONG_SCROLL_CIRCULAR
+    }
+
+    t.lastState = STATE_POSITION_MID
+    t.state = STATE_POSITION_MID
+
+    return t
+end
+
 
 ---@param root Object
 local function imageGroup(root, pos)
@@ -158,48 +247,48 @@ local function createWatchface(parent)
     wfRoot:add_flag(lvgl.FLAG.EVENT_BUBBLE)
 
     -- 背景
-    t.objImage = lvgl.Image(wfRoot,{x = 0, y = 0, src=imgPath("bg.bin")})
+    t.objImage = lvgl.Image(wfRoot, { x = 0, y = 0, src = imgPath("bg.bin") })
 
     -- 充电图标
-    t.chargeImg = Image(wfRoot, imgPath("ap.bin"), {61, 460})
+    t.chargeImg = Image(wfRoot, imgPath("ap.bin"), { 61, 460 })
 
     -- 电池电量
-    t.chargeCont = imageGroup(wfRoot, {0, 0})
-    t.chargeContChild1 = t.chargeCont:setChild(imgPath("s8.bin"), { x = 80, y = 457 });
-    t.chargeContChild2 = t.chargeCont:setChild(imgPath("s0.bin"), { x = 91, y = 457 });
-    t.chargeContChild3 = t.chargeCont:setChild(imgPath("s%.bin"), { x = 102, y = 457 });
-    t.chargeContChild4 = t.chargeCont:setChild(imgPath("s0.bin"), { x = 114, y = 457 });
+    t.chargeCont = imageGroup(wfRoot, { 0, 457 })
+    t.chargeContChild1 = t.chargeCont:setChild(imgPath("s8.bin"), { x = 80 });
+    t.chargeContChild2 = t.chargeCont:setChild(imgPath("s0.bin"), { x = 91 });
+    t.chargeContChild3 = t.chargeCont:setChild(imgPath("s%.bin"), { x = 102 });
+    t.chargeContChild4 = t.chargeCont:setChild(imgPath("s0.bin"), { x = 114 });
 
     -- 倒计时天数
-    t.timeDay = imageGroup(wfRoot, {0, 103})
-    t.timeDayChild1 = t.timeDay:setChild(imgPath("0.bin"), {x = 28, y = 0})
-    t.timeDayChild2 = t.timeDay:setChild(imgPath("9.bin"), {x = 60, y = 0})
-    t.timeDayChild3 = t.timeDay:setChild(imgPath("2.bin"), {x = 93, y = 0})
-    t.timeDayChild4 = t.timeDay:setChild(imgPath("8.bin"), {x = 125, y = 0})
+    t.timeDay = imageGroup(wfRoot, { 0, 103 })
+    t.timeDayChild1 = t.timeDay:setChild(imgPath("0.bin"), { x = 28 })
+    t.timeDayChild2 = t.timeDay:setChild(imgPath("9.bin"), { x = 60 })
+    t.timeDayChild3 = t.timeDay:setChild(imgPath("2.bin"), { x = 93 })
+    t.timeDayChild4 = t.timeDay:setChild(imgPath("8.bin"), { x = 125 })
 
     -- 小时分钟
-    t.timeHourHigh = Image(wfRoot, imgPath("0.bin"), {21, 303})
-    t.timeHourLow = Image(wfRoot, imgPath("9.bin"), {54, 303})
-    t.timeGang = Image(wfRoot, imgPath("say.bin"), {77, 303})
-    t.timeMinuteHigh = Image(wfRoot, imgPath("2.bin"), {100, 303})
-    t.timeMinuteLow = Image(wfRoot, imgPath("8.bin"), {133, 303})
+    t.timeHourHigh = Image(wfRoot, imgPath("0.bin"), { 21, 303 })
+    t.timeHourLow = Image(wfRoot, imgPath("9.bin"), { 54, 303 })
+    t.timeGang = Image(wfRoot, imgPath("say.bin"), { 77, 303 })
+    t.timeMinuteHigh = Image(wfRoot, imgPath("2.bin"), { 100, 303 })
+    t.timeMinuteLow = Image(wfRoot, imgPath("8.bin"), { 133, 303 })
 
     -- 星期
-    t.dateWeek = Image(wfRoot, imgPath("mon.bin"), {119, 384})
+    t.dateWeek = Image(wfRoot, imgPath("mon.bin"), { 119, 384 })
 
 
     -- 文字
-    t.gaokao = Image(wfRoot, imgPath("text1.bin"), {24, 54})
-    t.tian = Image(wfRoot, imgPath("text2.bin"), {84, 178})
-    t.xianzaishi = Image(wfRoot, imgPath("text3.bin"), {66, 256})
+    t.gaokao = Label(wfRoot, "", { w = 144, h = 56, x = 24, y = 54 }, "#ffffff")
+    t.tian = Image(wfRoot, imgPath("text2.bin"), { 84, 178 })
+    t.xianzaishi = Image(wfRoot, imgPath("text3.bin"), { 66, 256 })
 
     -- 日期
-    t.dateCont = imageGroup(wfRoot, {0, 0})
-    t.dateContChild1 = t.dateCont:setChild(imgPath("s0.bin"), { x = 28, y = 382 });
-    t.dateContChild2 = t.dateCont:setChild(imgPath("s8.bin"), { x = 40, y = 382 });
-    t.dateContChild3 = t.dateCont:setChild(imgPath("gang.bin"), { x = 50, y = 382 });
-    t.dateContChild4 = t.dateCont:setChild(imgPath("s1.bin"), { x = 60, y = 382 });
-    t.dateContChild5 = t.dateCont:setChild(imgPath("s6.bin"), { x = 71, y = 382 });
+    t.dateCont = imageGroup(wfRoot, { 0, 382 })
+    t.dateContChild1 = t.dateCont:setChild(imgPath("s0.bin"), { x = 28 });
+    t.dateContChild2 = t.dateCont:setChild(imgPath("s8.bin"), { x = 40 });
+    t.dateContChild3 = t.dateCont:setChild(imgPath("gang.bin"), { x = 50 });
+    t.dateContChild4 = t.dateCont:setChild(imgPath("s1.bin"), { x = 60 });
+    t.dateContChild5 = t.dateCont:setChild(imgPath("s6.bin"), { x = 71 });
 
     return t
 end
@@ -237,32 +326,35 @@ local function uiCreate()
 
         local s = 1
         if index < 10 then
+            watchface.chargeImg.widget:set({ x = 71 })
             src = string.format("s%d.bin", index)
-            watchface.chargeContChild1:set({ src = imgPath(src) })
+            watchface.chargeContChild1:set({ src = imgPath(src), x = 90 })
             watchface.chargeContChild1:clear_flag(lvgl.FLAG.HIDDEN)
-            watchface.chargeContChild2:set({ src = imgPath("s%.bin") })
+            watchface.chargeContChild2:set({ src = imgPath("s%.bin"), x = 102 })
             watchface.chargeContChild2:clear_flag(lvgl.FLAG.HIDDEN)
         elseif index < 100 then
+            watchface.chargeImg.widget:set({ x = 66 })
             src = string.format("s%d.bin", index // 10)
-            watchface.chargeContChild1:set({ src = imgPath(src) })
+            watchface.chargeContChild1:set({ src = imgPath(src), x = 85 })
             watchface.chargeContChild1:clear_flag(lvgl.FLAG.HIDDEN)
             src = string.format("s%d.bin", index % 10)
-            watchface.chargeContChild2:set({ src = imgPath(src) })
+            watchface.chargeContChild2:set({ src = imgPath(src), x = 96 })
             watchface.chargeContChild2:clear_flag(lvgl.FLAG.HIDDEN)
-            watchface.chargeContChild3:set({ src = imgPath("s%.bin") })
+            watchface.chargeContChild3:set({ src = imgPath("s%.bin"), x = 108 })
             watchface.chargeContChild3:clear_flag(lvgl.FLAG.HIDDEN)
             s = 2
         else
+            watchface.chargeImg.widget:set({ x = 61 })
             src = string.format("s%d.bin", 1)
-            watchface.chargeContChild1:set({ src = imgPath(src) })
+            watchface.chargeContChild1:set({ src = imgPath(src), x = 80 })
             watchface.chargeContChild1:clear_flag(lvgl.FLAG.HIDDEN)
             src = string.format("s%d.bin", 0)
-            watchface.chargeContChild2:set({ src = imgPath(src) })
+            watchface.chargeContChild2:set({ src = imgPath(src), x = 91 })
             watchface.chargeContChild2:clear_flag(lvgl.FLAG.HIDDEN)
             src = string.format("s%d.bin", 0)
-            watchface.chargeContChild3:set({ src = imgPath(src) })
+            watchface.chargeContChild3:set({ src = imgPath(src), x = 102 })
             watchface.chargeContChild3:clear_flag(lvgl.FLAG.HIDDEN)
-            watchface.chargeContChild4:set({ src = imgPath("s%.bin") })
+            watchface.chargeContChild4:set({ src = imgPath("s%.bin"), x = 114 })
             watchface.chargeContChild4:clear_flag(lvgl.FLAG.HIDDEN)
             s = 3
         end
@@ -286,13 +378,30 @@ local function uiCreate()
         obj:set { src = imgPath(src) }
     end)
 
-    -- 倒计时日期设置
-    local targetSeconds = os.time({ year = 2025, month = 6, day = 7, hour = 0, min = 0, sec = 0 })
+    local targetSeconds
 
     -- 倒计时天数
     dataman.subscribe("timeSecondLow", watchface.timeDay.widget, function(obj, value)
+        -- 倒计时日期设置
         now = os.time()
-        remainingSeconds = targetSeconds - now
+        if fileExists(file) then
+            local jsonStr = readFileToStr(file)
+            local time = splitString(splitString(jsonStr, ",")[2], "-")
+            targetSeconds = os.time({ year = time[1], month = time[2], day = time[3], hour = 0, min = 0, sec = 0 })
+            if targetSeconds - now > 0 then
+                watchface.gaokao.label:set({ text = "" .. splitString(jsonStr, ",")[1] .. "还有" })
+            else
+                watchface.gaokao.label:set({ text = "" .. splitString(jsonStr, ",")[1] .. "已过" })
+            end
+        else
+            targetSeconds = os.time({ year = 2025, month = 6, day = 7, hour = 0, min = 0, sec = 0 })
+            if targetSeconds - now > 0 then
+                watchface.gaokao.label:set({ text = "高考还有" })
+            else
+                watchface.gaokao.label:set({ text = "高考已过" })
+            end
+        end
+        remainingSeconds = math.abs(targetSeconds - now)
         days = math.ceil(remainingSeconds / (24 * 60 * 60)) -- 计算剩余天数
         watchface.timeDayChild1:add_flag(lvgl.FLAG.HIDDEN)
         watchface.timeDayChild2:add_flag(lvgl.FLAG.HIDDEN)
@@ -321,16 +430,16 @@ local function uiCreate()
             watchface.timeDayChild4:clear_flag(lvgl.FLAG.HIDDEN)
         else
             src = string.format("%d.bin", math.floor(days / 1000))
-            watchface.timeDayChild1:set({ src = imgPath(src),x = 28 })
+            watchface.timeDayChild1:set({ src = imgPath(src), x = 28 })
             watchface.timeDayChild1:clear_flag(lvgl.FLAG.HIDDEN)
             src = string.format("%d.bin", math.floor(days / 100) % 10)
-            watchface.timeDayChild2:set({ src = imgPath(src),x = 60 })
+            watchface.timeDayChild2:set({ src = imgPath(src), x = 60 })
             watchface.timeDayChild2:clear_flag(lvgl.FLAG.HIDDEN)
             src = string.format("%d.bin", math.floor(days / 10) % 10)
-            watchface.timeDayChild3:set({ src = imgPath(src),x = 93 })
+            watchface.timeDayChild3:set({ src = imgPath(src), x = 93 })
             watchface.timeDayChild3:clear_flag(lvgl.FLAG.HIDDEN)
             src = string.format("%d.bin", days % 10)
-            watchface.timeDayChild4:set({ src = imgPath(src),x = 125 })
+            watchface.timeDayChild4:set({ src = imgPath(src), x = 125 })
             watchface.timeDayChild4:clear_flag(lvgl.FLAG.HIDDEN)
         end
     end)
@@ -393,7 +502,7 @@ local on, off = uiCreate()
 
 function ScreenStateChangedCB(pre, now, reason)
     --printf("screen state", pre, now, reason)
-    if pre ~="ON" and now == "ON" then
+    if pre ~= "ON" and now == "ON" then
         on()
     elseif pre == "ON" and now ~= "ON" then
         off()
